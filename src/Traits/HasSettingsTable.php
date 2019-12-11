6a0c3ceba3e007cd618bebbb594735a99f5a1d5f
EricReichenbach <EricReichenbach@users.noreply.github.com>
2019-12-11 21:02:43 +0100

    getKey() instead of hardcoded id issue #36 (#37)
    * replace hardcoded ->id by the generic ->getKey() to support table with primary not named id

--- src/Traits/HasSettingsTable.php
+++ src/Traits/HasSettingsTable.php
@@ -61,8 +61,8 @@ trait HasSettingsTable
 
     public function getSettingsCacheKey(): string
     {
-        return config('model_settings.settings_table_cache_prefix') . $this->getTable() . '::' . $this->id;
+        return config('model_settings.settings_table_cache_prefix') . $this->getTable() . '::' . $this->getKey();
     }
-    
+
     abstract public function getTable();
 }
