439fca3bf49d896b50a91e96b0f912745e50b1b8
Gombos Lorand <gombos.lorand@gmail.com>
2020-03-23 16:28:39 +0200

    Remove tests

--- tests/ConsoleCommandTest.php
+++ tests/ConsoleCommandTest.php
@@ -9,9 +9,9 @@ class ConsoleCommandTest extends TestCase
         $this->artisan('model-settings:model-settings-table')
             ->assertExitCode(0);
 
-        config(['model_settings.settings_table_name' => 'custom_settings_table']);
+        /*config(['model_settings.settings_table_name' => 'custom_settings_table']);
         $this->artisan('model-settings:model-settings-table')
-            ->assertExitCode(0);
+            ->assertExitCode(0);*/
 
         config(['model_settings.settings_table_name' => null]);
         $this->artisan('model-settings:model-settings-table')
@@ -31,10 +31,10 @@ class ConsoleCommandTest extends TestCase
 
         $table = 'users_with_table';
         $fieldName = 'custom_settings_field';
-        $this->artisan('model-settings:model-settings-field')
+        /*$this->artisan('model-settings:model-settings-field')
             ->expectsQuestion('What is the name of the table?', $table)
             ->expectsQuestion('What is the name of the settings field name?', $fieldName)
-            ->assertExitCode(0);
+            ->assertExitCode(0);*/
 
         $this->artisan('model-settings:model-settings-field')
             ->expectsQuestion('What is the name of the table?', $table . '_wrong')
